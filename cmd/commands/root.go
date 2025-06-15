795544eb114469f0018de899dd4f81edd307fe01
Rishabh Jain <rjain23112002@gmail.com>
2025-06-15 15:43:03 +0530

    add: --debug flag and logging to a log file

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -1,7 +1,10 @@
 package commands
 
 import (
+	"log"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/rishabh-j-23/ex-crl/cmd/commands/add"
 	"github.com/rishabh-j-23/ex-crl/cmd/commands/project"
@@ -24,6 +27,35 @@ Features include:
 Use 'ex-crl project init' to nitialize a new project
 
 Use "ex-crl [command] --help" for more information on a specific command.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if cmd.Flag("debug").Changed {
+			log.SetOutput(os.Stdout)
+			log.SetFlags(log.LstdFlags | log.Lshortfile)
+		} else {
+			log.SetFlags(log.LstdFlags)
+
+			logDir := filepath.Join(os.Getenv("HOME"), "ex-crl", "logs")
+			logFileName := "ex-crl_" + time.Now().Format("2006-01-02") + ".log"
+			logPath := filepath.Join(logDir, logFileName)
+
+			// check if log directory exists, create it if not
+			if _, err := os.Stat(logDir); os.IsNotExist(err) {
+				err := os.MkdirAll(logDir, 0755)
+				if err != nil {
+					log.Printf("Failed to create log directory (%s): %v", logDir, err)
+				}
+			}
+
+			logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+			if err != nil {
+				// fallback to stdout if file can't be opened
+				log.SetOutput(os.Stdout)
+				log.Printf("Failed to open log file (%s), logging to stdout: %v", logPath, err)
+			} else {
+				log.SetOutput(logFile)
+			}
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,6 +69,8 @@ func Execute() {
 
 func init() {
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// add a debuf flag
+	RootCmd.PersistentFlags().BoolP("debug", "d", false, "debug mode")
 	RootCmd.AddCommand(add.AddCmd)
 	RootCmd.AddCommand(project.ProjectCmd)
 	RootCmd.AddCommand(workflow.WorkflowCmd)
